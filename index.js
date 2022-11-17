ca8fbb578819a0b664d9d0ae7d8ac13dc08867cf
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2022-11-16 21:24:22 -0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const Grafo = require('./grafo/grafo.js');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
